86115bb57a7c66b0f67b220427b8963f9327e6ce
Szymon Maruda <szymon.maruda@gmail.com>
2016-12-15 20:14:57 +0100

    index.html, style1.css and script1.js i script2.js was added

---
+++ script1.js
@@ -0,0 +1,5 @@
+var counter=0;
+function clicker(){
+  counter++;
+  document.getElementById("clickCount").innerHTML = counter;
+};
---
+++ script2.js
@@ -0,0 +1,4 @@
+function reset(){
+  setInterval(function(){
+    document.getElementById("clickCount").innerHTML=0; counter=0;}, 30000);
+}
